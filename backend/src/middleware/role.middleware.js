018551f96feb2831dffa95d0341daed02cfa4d4a
Mike <mike.luong06@gmail.com>
2026-05-07 04:29:39 +0300

    Fixed minor typerror on middleware role export

--- backend/src/middleware/role.middleware.js
+++ backend/src/middleware/role.middleware.js
@@ -1,5 +1,5 @@
-module.exports = (req, res, next) => {
-  if (req.user.role !== "admin") {
+module.exports = (requiredRole) => (req, res, next) => {
+  if (req.user.role !== requiredRole) {
     return res.status(403).json({ message: "Admin access required" });
   }
   next();
