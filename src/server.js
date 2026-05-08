3694d94198ee804ac1cb0ae33ed33604ce928cf9
Kasper Laine <50250247+LasperKaine@users.noreply.github.com>
2026-05-08 23:28:42 +0300

    add vercel

--- src/server.js
+++ src/server.js
@@ -2,6 +2,13 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
+const cors = require('cors');
+
+app.use(cors({
+  origin: ['https://citysushi-frontend.vercel.app', 'http://localhost:5173'],
+  credentials: true
+}));
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
