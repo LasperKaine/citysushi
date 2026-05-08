7f457e05f178d7365dc79292b74a725ead61fa97
Kasper Laine <50250247+LasperKaine@users.noreply.github.com>
2026-05-08 23:36:15 +0300

    updated cors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://citysushi-frontend.vercel.app', 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 
 const authRoutes = require("./routes/auth.routes");
